43cb7f6da1602aa39949db59f70df8012632ba62
takaya456 <5milingmarineday@gmail.com>
2021-09-19 23:55:53 +0900

    コントローラーとモデル、マイグレーションファイルを作成した

--- database/migrations/2021_09_19_081124_create_direct_messages_table.php
+++ database/migrations/2021_09_19_081124_create_direct_messages_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateUsersTable extends Migration
+class CreateDirectMessagesTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,11 +13,13 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('direct_messages', function (Blueprint $table) {
             $table->bigIncrements('id');
-            $table->string('username', 20);
-            $table->string('password', 100);
-            $table->text('imagePath')->nullable();
+            $table->unsignedBigInteger('to');
+            $table->foreign('to')->references('id')->on('users');
+            $table->unsignedBigInteger('from');
+            $table->foreign('from')->references('id')->on('users');
+            $table->text('content');
             $table->timestamps();
         });
     }
@@ -29,6 +31,6 @@ class CreateUsersTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('direct_messages');
     }
 }
